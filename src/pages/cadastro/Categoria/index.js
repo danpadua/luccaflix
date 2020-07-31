0ab578d0ea618c73416b6625ee6253a15fda6dc1
Danilo <dpaduaoliveira@gmail.com>
2020-07-31 16:42:07 -0300

    Implementação da tela de cadastro de categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,24 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+    const [categorias, setCategoria] = useState([]);
+    const formData = { nome: '', descricao: '', cor: '#FF0000' }
+    const [formCategoria, setFormCategoria] = useState(formData);
+
+    function setFormData(key, value) {
+        setFormCategoria({
+            ...formCategoria,
+            [key]: value
+        })
+    }
+
+    function handleChange(e) {
+        setFormData(
+            e.target.getAttribute('name'),
+            e.target.value
+        );
+    }
+
     return (
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
 
-            <form>
-                <label>
-                Nome da Categoria:
-                <input
+            <form onSubmit={(e) =>{
+                e.preventDefault();
+                setCategoria([
+                    ...categorias,
+                    formCategoria.nome
+                ]);
+
+                setFormCategoria(formData);
+            }}>
+
+                <FormField
+                    label="Nome da Categoria"
                     type="text"
+                    name="nome"
+                    required
+                    value={formCategoria.nome}
+                    onChange={handleChange}
                 />
-                </label>
+
+                <FormField
+                    label="Descrição"
+                    type="textArea"
+                    name="descricao"
+                    value={formCategoria.descricao}
+                    onChange={handleChange}
+                />
+
+                <FormField
+                    label="Cor"
+                    type="color"
+                    name="cor"
+                    value={formCategoria.cor}
+                    onChange={handleChange}
+                />
 
                 <button>
-                Cadastrar
+                    Cadastrar
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria, idx) => {
+                    console.log(categoria);
+                    return(
+                        <li key={idx}>{categoria}</li>
+                    )
+                })}
+            </ul>
 
             <Link to="/">
                 Ir para Home
